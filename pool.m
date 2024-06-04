78e0e033948c1aa19ef83ea42cdbb3a6bfc8b100
Cassandra-EC <162471376+Cassandra-EC@users.noreply.github.com>
2024-06-04 12:19:25 +0100

    Forming files
    3 hrs planning + initializing files
    
    Need to create vectors w appropriate # of each letter; maybe 3 'bags' for the player to choose from for which they can also have their stats saved

---
+++ pool.m
@@ -0,0 +1,9 @@
+% created 6/4/24
+
+function pool
+    % displays total letters
+
+
+
+
+end
---
+++ selection.m
@@ -0,0 +1,10 @@
+function choices = selection
+
+
+    %%% OUTPUT: 1) letters_total (num letters in play); 2) length_min (minimum word length) 
+
+    
+choices = 3;
+
+
+end
