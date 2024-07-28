58454254aa69fb0eb3e12ec3e740d62d1092f94d
Cassandra-EC <162471376+Cassandra-EC@users.noreply.github.com>
2024-07-28 12:43:39 +0100

    1) learning more about app designer and 2) making progress on the snatch-it app
    ? hours (5-10), forgot to make pushes. Some from videos and doing short tutorials. Progress on the snatch-it app

--- start.m
+++ start.m
@@ -9,6 +9,11 @@ start_round = 1;
 tile_bag = generate_tile_bag;
 pool = {};
 
-% do this as many times as user clicks
-[pool, tile_bag] = add_to_pool(pool, tile_bag);
-disp(pool);
+% x = 0;
+% % do this as many times as user clicks
+% if x == true
+%     [pool, tile_bag] = add_to_pool(pool, tile_bag);
+%     disp(pool);
+% end
+
+
